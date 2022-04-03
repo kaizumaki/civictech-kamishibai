f8a2916756d4a9c0dbd8f3996d70cb190fed14e4
Kazuki Imamura <kaizumaki@gmail.com>
2022-04-03 12:03:01 +0900

    キーワードのセレクトボックスを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import defaultKeywords from './data/keywords.json';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import {TextField} from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 'auto',
+    },
+  },
+};
 
 const getRandomNumber = (max: number, min: number = 0): number => {
   return Math.floor(Math.random() * (max - min) + min)
@@ -8,13 +28,23 @@ const getRandomNumber = (max: number, min: number = 0): number => {
 
 function App() {
   const [keyword, setKeyword] = useState('');
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
   const [itemSrc, setItemSrc] = useState('');
   const [itemAttribution, setItemAttribution] = useState('');
 
-  async function handleChangeItem() {
-    const keywordArray = keyword.split(',');
-    const selectedKeyword = keywordArray[getRandomNumber(keywordArray.length)].trim();
-    const response: any = await axios.get(`https://api.openverse.engineering/v1/images/?q="${selectedKeyword}"&license=by,cc0&page_size=50`);
+  const handleChangeKeyword = (e: SelectChangeEvent<typeof selectedKeywords>) => {
+    const {
+      target: { value },
+    } = e;
+    setSelectedKeywords(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  }
+
+  const handleChangeItem = async () => {
+    const keywordArray = [...selectedKeywords, ...keyword.split(',')].filter(v => v);
+    const targetKeyword = keywordArray[getRandomNumber(keywordArray.length)].trim();
+    const response: any = await axios.get(`https://api.openverse.engineering/v1/images/?q="${targetKeyword}"&license=by,cc0&page_size=50`);
     const results = response.data.results;
     const url = results[getRandomNumber(results.length)].detail_url;
     const updatedURL = url.replace(/^http:\/\//i, 'https://');
@@ -35,19 +65,49 @@ function App() {
       </div>
       <div className="form-container">
         <div className="form-item">
-          <input
-            className="keyword-field"
-            type="text"
-            placeholder="e.g. democracy, open source, blue sky"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
-          />
+          <FormControl sx={{ m: 2, width: '100%', maxWidth: 600 }}>
+            <InputLabel id="default-keyword-label">Keyword</InputLabel>
+            <Select
+              labelId="default-keyword-label"
+              id="default-keyword"
+              multiple
+              value={selectedKeywords}
+              onChange={handleChangeKeyword}
+              input={<OutlinedInput id="select-default-keyword" label="Keyword" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {defaultKeywords.map((item, index) => (
+                <MenuItem
+                  key={index}
+                  value={item['keyword-en']}
+                >
+                  {`${item['keyword-ja']}（${item['keyword-en']}）`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ m: 2, width: '100%', maxWidth: 600 }}>
+            <TextField
+              fullWidth
+              label="Additional keywords"
+              placeholder="e.g. cat, dog, blue sky"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+          </FormControl>
         </div>
         <div className="form-item">
           <button
             className="next-button"
             type="button"
-            disabled={keyword === ''}
+            disabled={selectedKeywords.length === 0 && keyword === ''}
             onClick={() => handleChangeItem()}
           >
             Next!
